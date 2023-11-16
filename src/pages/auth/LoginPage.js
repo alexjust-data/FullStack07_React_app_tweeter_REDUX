3c43f809e88241716341d14b0d8ce2acee7b35b2
davidjj76 <davidjj76@gmail.com>
2023-11-16 19:39:49 +0100

    Control inputs

--- src/pages/auth/LoginPage.js
+++ src/pages/auth/LoginPage.js
@@ -1,25 +1,49 @@
+import { useState } from 'react';
 import Button from '../../components/Button';
 import { login } from './service';
 
 function LoginPage({ onLogin }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   const handleSubmit = async event => {
     event.preventDefault();
 
     await login({
-      username: event.target.username.value,
-      password: event.target.password.value,
+      username,
+      password,
     });
 
     onLogin();
   };
 
+  const handleUsernameChange = event => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = event => {
+    setPassword(event.target.value);
+  };
+
+  const disabled = !(username && password);
+
   return (
     <div>
       <h1>Log in to Twitter</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="username" />
-        <input type="password" name="password" />
-        <Button type="submit" $variant="primary">
+        <input
+          type="text"
+          name="username"
+          onChange={handleUsernameChange}
+          value={username}
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={handlePasswordChange}
+          value={password}
+        />
+        <Button type="submit" $variant="primary" disabled={disabled}>
           Log in
         </Button>
       </form>
