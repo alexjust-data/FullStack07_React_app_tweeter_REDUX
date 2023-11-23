ced2bcb44f8f681fce6a61b73643fd6566c29bd7
davidjj76 <davidjj76@gmail.com>
2023-11-23 19:44:47 +0100

    Lazy loading and Suspense

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { Suspense, lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import Layout from './components/layout/Layout';
-import LoginPage from './pages/auth/LoginPage';
 import TweetsPage from './pages/tweets/TweetsPage';
 import NewTweetPage from './pages/tweets/NewTweetPage/NewTweetPage';
 import TweetPage from './pages/tweets/TweetPage';
 import RequireAuth from './pages/auth/components/RequireAuth';
 
+// import LoginPage from './pages/auth/LoginPage';
+const LoginPage = lazy(() => import('./pages/auth/LoginPage'));
+
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/login"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <LoginPage />
+          </Suspense>
+        }
+      />
       <Route path="/tweets" element={<Layout />}>
         <Route index element={<TweetsPage />} />
         <Route path=":tweetId" element={<TweetPage />} />
