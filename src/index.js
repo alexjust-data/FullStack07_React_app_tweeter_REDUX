6ba5c7e8ec7a22ca2b807d740492b06fdb3f96ef
davidjj76 <davidjj76@gmail.com>
2024-01-16 21:54:08 +0100

    Create store, actions, reducer, types

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import { setAuthorizationHeader } from './api/client';
 import { AuthContextProvider } from './pages/auth/context';
 import ErrorBoundary from './components/errors/ErrorBoundary';
 
-import './store';
+import configureStore from './store';
+
+const store = configureStore();
 
 const accessToken = storage.get('auth');
 if (accessToken) {
---
+++ src/store/index-poc.js
@@ -0,0 +1,61 @@
+// import { createStore } from 'redux';
+
+function createStore(reducer) {
+  let state;
+  let listeners = [];
+
+  function getState() {
+    return state;
+  }
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    listeners.forEach(l => l());
+  }
+
+  function subscribe(listener) {
+    listeners.push(listener);
+
+    return function () {
+      listeners = listeners.filter(l => !listener);
+    };
+  }
+
+  dispatch({ type: 'initialization' });
+
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  };
+}
+
+const INCREMENT = 'increment';
+const DECREMENT = 'decrement';
+
+const increment = () => ({ type: INCREMENT });
+const decrement = payload => ({ type: DECREMENT, payload });
+
+const reducer = (state = 0, action) => {
+  // implement state logic
+  switch (action.type) {
+    case INCREMENT:
+      return state + (action.payload || 1);
+    case DECREMENT:
+      return state - (action.payload || 1);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+const callback = () => console.log('state', store.getState());
+const unsubscribe = store.subscribe(callback);
+callback();
+
+store.dispatch(increment());
+
+unsubscribe();
+store.dispatch(decrement(5));
+console.log(store.getState());
